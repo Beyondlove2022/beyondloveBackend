63898f3ca0dd8bcd10f2e71667cbf0886d952027
Harsha441 <harshavardhanb22@gmail.com>
2022-08-01 17:11:14 +0530

    pet profile picture functions -- Harsha

--- Controllers/Pet.js
+++ Controllers/Pet.js
@@ -1,4 +1,5 @@
 import Pet from "../Models/customer/pet.js";
+import { uploadfile } from "../Utils/s3Bucket.js";
 
 export const createPetProfile = async (req, res) => {
   const customerId = req.user.id;
@@ -44,14 +45,14 @@ export const createPetProfile = async (req, res) => {
   }
 };
 
-export const updatePetProfile = async(req, res) => {
+export const updatePetProfile = async (req, res) => {
   const id = req.user.id;
-  const {petId} = req.params;
+  const { petId } = req.params;
   console.log("petId:", petId);
   try {
-    const pet = await Pet.findByIdAndUpdate({_id: petId}, {$set: req.body});
+    const pet = await Pet.findByIdAndUpdate({ _id: petId }, { $set: req.body });
     const petProfile = Pet.findById(petId);
-    return res.json({success: true, msg: "Pet Profile updated successfully"});
+    return res.json({ success: true, msg: "Pet Profile updated successfully" });
   } catch (error) {
     console.log(error);
     return res.json({ success: false, msg: "Something went wrong" });
@@ -59,11 +60,15 @@ export const updatePetProfile = async(req, res) => {
 };
 
 export const getUniquePetProfile = async (req, res) => {
-  const {petId} = req.params;
+  const { petId } = req.params;
   try {
     const pet = await Pet.findById(petId);
-    if(!pet) return res.json({success: false, msg: "Pet Data Not Found"});
-    return res.json({success: true, msg: "Pet Profile Sended SuccessFully", pet})
+    if (!pet) return res.json({ success: false, msg: "Pet Data Not Found" });
+    return res.json({
+      success: true,
+      msg: "Pet Profile Sended SuccessFully",
+      pet,
+    });
   } catch (error) {
     console.log(error);
     return res.json({ success: false, msg: "Something went wrong" });
@@ -71,12 +76,13 @@ export const getUniquePetProfile = async (req, res) => {
 };
 
 export const getAllPetProfilebyCustomer = async (req, res) => {
-  const {customerId} = req.params;
+  const { customerId } = req.params;
   try {
-    const customerPets = await Pet.find({customerId});
+    const customerPets = await Pet.find({ customerId });
     console.log("pets", customerPets);
-    if(customerPets.length < 0) return res.json({success: false, msg: "Pets Not Found"});
-    return res.json({success: true, msg: "Pets data Sended", customerPets});
+    if (customerPets.length < 0)
+      return res.json({ success: false, msg: "Pets Not Found" });
+    return res.json({ success: true, msg: "Pets data Sended", customerPets });
   } catch (error) {
     console.log(error);
     return res.json({ success: false, msg: "Something went wrong" });
@@ -86,10 +92,47 @@ export const getAllPetProfilebyCustomer = async (req, res) => {
 export const getAllPetProfile = async (req, res) => {
   try {
     const pets = await Pet.find();
-    return res.json({success: true, msg: "Overall Pets Data", pets});
+    return res.json({ success: true, msg: "Overall Pets Data", pets });
   } catch (error) {
     console.log(error);
     return res.json({ success: false, msg: "Something went wrong" });
   }
 };
 
+export const uploadPetProfilePic = async (req, res) => {
+  const file = req.files;
+  const customerId = req.user.id;
+  const petId = req.params;
+  try {
+    const result = await uploadfile(file, customerId, petId);
+    const profilePic = result.Key;
+    await Pet.findByIdAndUpdate({ _id: id }, { profilePic });
+    const pet = await Pet.findById(petId);
+    return res.json({
+      success: true,
+      msg: "Profile Image uploaded successfully",
+      pet,
+    });
+  } catch (error) {
+    return res.json({ success: false, msg: "Something went wrong", error });
+  }
+};
+
+export const getPetProfilePhoto = async (req, res, next) => {
+  try {
+    const key = req.params.id;
+    console.log(key, "key");
+    if (key != "undefined") {
+      const readStream = await downloadFile(key);
+      return await readStream.pipe(res);
+    } else {
+      return console.log(key);
+    }
+  } catch (error) {
+    return req.json({
+      success: false,
+      msg: "Something went wrong",
+      error: error,
+    });
+  }
+};
--- Routes/Pet.js
+++ Routes/Pet.js
@@ -3,20 +3,36 @@ import {
   createPetProfile,
   getAllPetProfile,
   getAllPetProfilebyCustomer,
+  getPetProfilePhoto,
   getUniquePetProfile,
   updatePetProfile,
+  uploadPetProfilePic,
 } from "../Controllers/Pet.js";
 import { customerAuthentication } from "../Middleware/authentication.js";
 
 const route = express.Router();
 
-route.post("/customer/pet/create/:token", customerAuthentication, createPetProfile);
-route.put("/customer/pet/update/:petId/:token", customerAuthentication, updatePetProfile);
+route.post(
+  "/customer/pet/create/:token",
+  customerAuthentication,
+  createPetProfile
+);
+route.put(
+  "/customer/pet/update/:petId/:token",
+  customerAuthentication,
+  updatePetProfile
+);
 route.get("/customer/pet/get-unique-profile/:petId", getUniquePetProfile);
 route.get(
   "/customer/pet/get-allprofiles-by-customer/:customerId",
   getAllPetProfilebyCustomer
 );
 route.get("/customer/pet/get-allprofiles", getAllPetProfile);
+route.put(
+  "/customer/pet/upload-profile-pic/:token/:petId",
+  customerAuthentication,
+  uploadPetProfilePic
+);
+route.get("/customer/pet/get-pet-profile-pic/:id", getPetProfilePhoto);
 
 export default route;
