065952be739aa067e8b8b99bdae7a415ec534844
Harsha441 <harshavardhanb22@gmail.com>
2022-09-06 12:03:44 +0530

    Customers

--- Routes/Customer.js
+++ Routes/Customer.js
@@ -1,11 +1,12 @@
 import express from "express";
 import {
-  createAppointment,
-  customerLogin,
-  customerRegister,
-  customerRegisterWithOtp,
-  getCustomerProfile,
-  updateCustomerProfile,
+	createAppointment,
+	customerLogin,
+	customerRegister,
+	customerRegisterWithOtp,
+	getAllCustomers,
+	getCustomerProfile,
+	updateCustomerProfile,
 } from "../Controllers/customer.js";
 import { customerAuthentication } from "../Middleware/authentication.js";
 
@@ -15,15 +16,16 @@ route.post("/customer/register-otp/:number", customerRegisterWithOtp);
 route.post("/customer/register", customerRegister);
 route.post("/customer/login", customerLogin);
 route.put(
-  "/customer/update-profile/:token",
-  customerAuthentication,
-  updateCustomerProfile
+	"/customer/update-profile/:token",
+	customerAuthentication,
+	updateCustomerProfile
 );
 route.get("/customer/get-profile/:id", getCustomerProfile);
 route.post(
-  "/customer/appointment-booking/:token",
-  customerAuthentication,
-  createAppointment
+	"/customer/appointment-booking/:token",
+	customerAuthentication,
+	createAppointment
 );
+route.get("/customer/get-all-customers", getAllCustomers);
 
 export default route;
