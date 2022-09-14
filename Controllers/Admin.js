fb9a3913da97d77521fe694ac9e7417deade442a
SridharIyappan <sridharsp4774@gmail.com>
2022-09-14 11:29:28 +0530

    Something

--- Controllers/Admin.js
+++ Controllers/Admin.js
@@ -8,6 +8,7 @@ import PetFood from "../Models/petFood.js";
 import PetGrooming from "../Models/petGrooming.js";
 import PetTraining from "../Models/petTraining.js";
 import { generateToken } from "../Utils/jwtToken.js";
+import { updateCount } from "./count.js";
 
 export const createAdmin = async (req, res) => {
   let { email, password, mobile } = req.body;
@@ -87,6 +88,82 @@ export const getUniqueCustomer = async (req, res) => {
   }
 };
 
+export const createBussinessAccount = async (req, res) => {
+  let {
+    businessName,
+    email,
+    mobile,
+    password,
+    category,
+    address,
+    location,
+    city,
+    state,
+  } = req.body;
+  let categoryName;
+  try {
+    if (category == "PetClinic") {
+      categoryName = PetClinic;
+    } else if (category == "PetBoarding") {
+      categoryName = PetBoarding;
+    } else if (category == "PetGrooming") {
+      categoryName = PetGrooming;
+    } else if (category == "PetTraining") {
+      categoryName = PetTraining;
+    } else if (category == "PetFood") {
+      categoryName = PetFood;
+    } else {
+      return res.json({ success: false, msg: "This category not available" });
+    }
+    let findEmail = await categoryName.findOne({ email });
+    let findMobile = await categoryName.findOne({ mobile });
+    console.log({ findMobile });
+
+    if (findEmail || findMobile) {
+      if (findEmail && findMobile)
+        return res.json({
+          success: false,
+          msg: "Email And Mobile Number Already Exist",
+        });
+      if (findEmail)
+        return res.json({ success: false, msg: "Email Already Exist" });
+      if (findMobile)
+        return res.json({
+          success: false,
+          msg: "Mobile Number Already Exist",
+        });
+    }
+    const salt = await bcrypt.genSalt(10);
+    password = await bcrypt.hash(password, salt);
+
+    let business = await new categoryName({
+      businessName,
+      password,
+      mobile,
+      email,
+      address,
+      location,
+      city,
+      state,
+    });
+    console.log({ business });
+    await business.save();
+    const tok = { id: business._id };
+    const token = await generateToken(tok);
+    let businessDetails = await categoryName.findById(business._id);
+    await updateCount(category);
+    return res.json({
+      success: true,
+      msg: "Business Account Created Successfully",
+      businessDetails,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({ success: false, msg: "something went wrong" });
+  }
+};
+
 export const editBussinessAccount = async (req, res) => {
   const { category, businessId } = req.body;
   let categoryName;
@@ -123,12 +200,10 @@ export const editBussinessAccount = async (req, res) => {
   }
 };
 
-export const blockOrUnblockBusiness = async (req, res) => {
+export const deleteBusinessAccount = async (req, res) => {
   const { businessId, category } = req.body;
   let categoryName;
-  const countId = process.env.COUNT_ID;
   try {
-    let count = await Count.findById(countId);
     if (category == "PetClinic") {
       categoryName = PetClinic;
     } else if (category == "PetBoarding") {
@@ -145,10 +220,67 @@ export const blockOrUnblockBusiness = async (req, res) => {
     const business = await categoryName.findById(businessId);
     if (!business)
       return res.json({ success: false, msg: "Profile Not Found" });
-    await categoryName.findByIdAndUpdate(
-      { _id: businessId },
-      { block: !business.block }
-    );
+    const deleteBusiness = await categoryName.findByIdAndDelete(businessId);
+    return res.json({ success: true, msg: "Account Deleted Successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.json({ success: false, msg: "Something Went Wrong" });
+  }
+};
+
+export const blockOrUnblockBusiness = async (req, res) => {
+  const { businessId, category, block } = req.body;
+  let categoryName;
+  const countId = process.env.COUNT_ID;
+  try {
+    let count = await Count.findById(countId);
+    let val;
+    console.log(block);
+    if (block == "true") {
+      console.log("if");
+      val = 1;
+    } else {
+      val = -1;
+      console.log("else");
+    }
+    console.log({ val });
+    if (category == "PetClinic") {
+      categoryName = PetClinic;
+      await Count.findByIdAndUpdate(
+        { _id: countId },
+        { $inc: { blockedServiceProviders: val, blockedClinics: val } }
+      );
+    } else if (category == "PetBoarding") {
+      categoryName = PetBoarding;
+      await Count.findByIdAndUpdate(
+        { _id: countId },
+        { $inc: { blockedServiceProviders: val, blockedBoardings: val } }
+      );
+    } else if (category == "PetGrooming") {
+      categoryName = PetGrooming;
+      await Count.findByIdAndUpdate(
+        { _id: countId },
+        { $inc: { blockedServiceProviders: val, blockedGroomings: val } }
+      );
+    } else if (category == "PetTraining") {
+      categoryName = PetTraining;
+      await Count.findByIdAndUpdate(
+        { _id: countId },
+        { $inc: { blockedServiceProviders: val, blockedTrainings: val } }
+      );
+    } else if (category == "PetFood") {
+      categoryName = PetFood;
+      await Count.findByIdAndUpdate(
+        { _id: countId },
+        { $inc: { blockedServiceProviders: val, blockedFoods: val } }
+      );
+    } else {
+      return res.json({ success: false, msg: "This category not available" });
+    }
+    const business = await categoryName.findById(businessId);
+    if (!business)
+      return res.json({ success: false, msg: "Profile Not Found" });
+    await categoryName.findByIdAndUpdate({ _id: businessId }, { block: block });
     const businessDetails = await categoryName.findById(businessId);
     return res.json({
       success: true,
@@ -222,5 +354,14 @@ export const verifyBusiness = async (req, res) => {
   } catch (error) {
     console.log(error);
     return res.json({ success: false, msg: "Something Went Wrong", error });
+  }
+};
+
+export const getStateProviderCount = async (req, res) => {
+  try {
+    const getStateCount = await PetClinic.find();
+  } catch (error) {
+    console.log(error);
+    return res.json({ success: false, msg: "Something Went Wrong" });
   }
 };
